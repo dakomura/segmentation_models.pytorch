a73194a9310189067438548c36ef5476a69978f8
Daisuke Komura <kdais-prm@m.u-tokyo.ac.jp>
2021-05-28 21:45:25 +0900

    can load pretrained weights from file

--- segmentation_models_pytorch/encoders/__init__.py
+++ segmentation_models_pytorch/encoders/__init__.py
@@ -54,7 +54,11 @@ def get_encoder(name, in_channels=3, depth=5, weights=None):
             raise KeyError("Wrong pretrained weights `{}` for encoder `{}`. Available options are: {}".format(
                 weights, name, list(encoders[name]["pretrained_settings"].keys()),
             ))
-        encoder.load_state_dict(model_zoo.load_url(settings["url"]))
+	if settings["url"].startswith('http'):
+		encoder.load_state_dict(model_zoo.load_url(settings["url"]))
+	else:
+		encoder.load_state_dict(settings["url"])
+
 
     encoder.set_in_channels(in_channels)
 
